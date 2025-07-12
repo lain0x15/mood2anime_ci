6e000152d8b321c28722a24b27a2d546e81cd6a6
Lain0x15 <max0x15@outlook.com>
2025-07-12 03:22:56 +0300

    fx

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ python3 manage.py migrate && \
   python3 manage.py collectstatic --clear --noinput && \
   python3 manage.py import --path "$DJANGO_IMPORT_PATH"
 
-python3 -m gunicorn --bind "127.0.0.1:8080" mood2anime.wsgi
+python3 -m gunicorn --bind "0.0.0.0:8080" mood2anime.wsgi
